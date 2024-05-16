0a97ee4a66315011bcc2adafb2c10feb424fb1c3
mabroukissam <145654259+MabroukIssam@users.noreply.github.com>
2024-05-16 11:58:10 +0100

    the book view/ controller  is updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express', myVariable:'A variable to test' });
+// });
+
+// GET home page.
+router.get("/", function (req, res) {
+  res.redirect("/catalog");
 });
+
+// router.get("/catalog", function (req, res) {
+//   res.render("catalog");
+// });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var coolRouter = require("./cool");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/cool',(req,res)=>
-{
-  res.send('you are cool minzo')
-})
+router.use("/cool", coolRouter);
+// router.get('/cool',(req,res)=>
+// {
+//   res.send('you are cool minzo')
+// })
 module.exports = router;
